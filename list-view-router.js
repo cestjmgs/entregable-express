aec9f4887007cb849a70892fda573d9f9c69bb62
Juliana <juliana.go.ma.sa@gmail.com>
2023-12-06 11:13:25 -0500

    middleware manejo de errores, middleware gestión de solicitudes métodos http, middleware gestión de parámetros

--- list-view-router.js
+++ list-view-router.js
@@ -1,16 +1,31 @@
 //Importar el módulo de express
 const express = require('express');
 const listViewRouter = express.Router();
+const app = express();
+
+app.use(express.json());
+
+//Middleware para gestionar parámetros correctos
+const handleParams = (req, res, next) => { 
+    const tareaId = req.params.tareaId; 
+
+    if(isNaN(tareaId)) {
+    return res.status(400).json('Parámetro no válido. Debe ser un número.');
+    }; 
+    next();
+
+};
+
 
 //Ruta para las tareas completadas
-listViewRouter.get('/tareascompletadas', (req, res) => 
+listViewRouter.get('/tareascompletadas', handleParams, (req, res) => 
 {
     const tareasCompletadas = tasks.filter((task) => task.completed);
-    res.json(tareasCompletadas);
+    res.status(200).json(tareasCompletadas);
 })
 
 //Ruta para las tareas por completar
-listViewRouter.get('/tareasporcompletar', (req, res) => 
+listViewRouter.get('/tareasporcompletar', handleParams, (req, res) => 
 {
     const tareasPorCompletar = tasks.filter((task) => !task.completed);
     res.json(tareasPorCompletar);
